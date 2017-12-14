300b02a9f18fab6d8ff35b6bbd65a564937576f9
Christophe Dospital <christophe.dospital@matsiya.com>
2017-12-14 17:42:23 +0100

    ProductConstant -> product properties update

--- Models/ProductConstants.cs
+++ Models/ProductConstants.cs
@@ -36,24 +36,38 @@ namespace voltaire.Models
 
                 }
             });
-            Products.Add(new Product() { Description = "This is a accessories", Name = "Accessories" });
+            Products.Add(new Product()
+            {
+                Description = "This is a accessories",
+                Name = "Accessories",
+                Properties = new List<ProductProperty>()
+                {
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Category" , PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Name", PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Sale price", PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Note", PropertyValue = null },
+                }
+            }); //Normally Note is an Editor
+
             Products.Add(new Product()
             {
                 Description = "This is a service",
                 Name = "Service",
                 Properties = new List<ProductProperty>()
                 {
-                    new ProductProperty(PropertyType.IsBoolean){ PropertyName = "RBQ soft", PropertyValue = "false" },
-                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Seat*", PropertyValue = null, ItemSource = SeatTypes },
-                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Tree*", PropertyValue = "Mahogany", ItemSource = TreeTypes },
-                    new ProductProperty(PropertyType.IsText){ PropertyName = "Rider Name" , PropertyValue = null },
-                    new ProductProperty(PropertyType.IsBoolean){ PropertyName = "Voltaire Keeper", PropertyValue = "true" },
-                    new ProductProperty(PropertyType.IsBoolean){ PropertyName = "2nd Skin", PropertyValue = "false" },
-                    new ProductProperty(PropertyType.IsBoolean){ PropertyName = "RBQ soft", PropertyValue = "false" },
-                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Seat*", PropertyValue = null, ItemSource = SeatTypes },
-                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Tree*", PropertyValue = "Mahogany", ItemSource = TreeTypes }
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Serail number", PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Brand", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Model", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Seat size", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Flap size", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Leather", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Color", PropertyValue = null, ItemSource = ColorTypes },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Category", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Comments", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Sale price", PropertyValue = null},
                 }
-            });
+            }); //Normally Comments is an editor
+
             Products.Add(new Product()
             {
                 Description = "This is  others",
@@ -65,9 +79,47 @@ namespace voltaire.Models
                     new ProductProperty(PropertyType.IsText){ PropertyName = "Description", PropertyValue = null}
                 }
 
-            });
-            Products.Add(new Product() { Description = "This is a tradein", Name = "TradeIn" });
-            Products.Add(new Product() { Description = "This is a discount", Name = "Discount" });
+            }); //Normally Description is an editor
+
+            Products.Add(new Product()
+            {
+                Description = "This is a tradein",
+                Name = "TradeIn",
+                Properties = new List<ProductProperty>()
+                {
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Serail number", PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Brand", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Model", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Size", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Flap", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsPicker){ PropertyName = "Color", PropertyValue = null, ItemSource = ColorTypes },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Leather", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsBoolean){ PropertyName = "Blocks", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Other", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Amount", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Note", PropertyValue = null},
+                }
+
+            }); //Normally N is an editor
+
+            Products.Add(new Product()
+            {
+                Description = "This is a discount",
+                Name = "Discount",
+                Properties = new List<ProductProperty>()
+                {
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Name", PropertyValue = null },
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Sale Price", PropertyValue = null},
+                    new ProductProperty(PropertyType.IsText){ PropertyName = "Note", PropertyValue = null}
+                }
+            }); //Normally Note is an editor
+
+
+
+
+
+
+
 
             Agreements.Add(new Agreement() { Title = "1) You will have to obey to all the rules and regulations mentioned by the company." });
             Agreements.Add(new Agreement() { Title = "2) You will have to obey to all the rules and regulations mentioned by the company." });
@@ -91,6 +143,8 @@ namespace voltaire.Models
         public static List<Product> Products { get; set; } = new List<Product>() { };
 
         public static List<Agreement> Agreements { get; set; } = new List<Agreement>();
+
+        public static List<string> ColorTypes { get; set; } = new List<string>() { "Light brown", "Chocolate", "Black" };
 
         public static List<string> SeatTypes { get; set; } = new List<string>() { "Big", "Small", "Medium", "Extra Large" };
 
