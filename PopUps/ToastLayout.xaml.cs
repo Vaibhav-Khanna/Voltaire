ae32a5eb9aabf042a60062f54d830d6487c56052
Vaibhav Khanna <y2kvaibhav@gmail.com>
2017-12-18 18:19:22 +0530

    Loader Changes

--- PopUps/ToastLayout.xaml.cs
+++ PopUps/ToastLayout.xaml.cs
@@ -18,9 +18,25 @@ namespace voltaire.PopUps
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
+         
             anim.Play();
+            Device.StartTimer(new TimeSpan(0, 0, 2), HandleFunc);
         }
 
+        bool HandleFunc()
+        {
+            Device.BeginInvokeOnMainThread( () => 
+            {
+                container.Padding = 0;
+                container.HeightRequest = 20;
+                anim.HeightRequest = 20;
+                anim.WidthRequest = 20;
+                anim.HorizontalOptions = LayoutOptions.Center;
+                anim.TranslationX = -100;
+                ToastLabel.Text = "";
+            });
+            
+            return false;
+        }
     }
 }
