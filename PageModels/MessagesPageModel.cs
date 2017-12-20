d33dfc9f72cc883e7526440f6f6dc155054a686a
Christophe Dospital <christophe.dospital@matsiya.com>
2017-12-20 15:32:56 +0100

    message for order add (html support on commentary)

--- PageModels/MessagesPageModel.cs
+++ PageModels/MessagesPageModel.cs
@@ -16,17 +16,16 @@ namespace voltaire.PageModels
         public Command AddMessage => new Command(async (obj) =>
        {
            var currUser = await StoreManager.UserStore.GetCurrentUserAsync();
-
-           var message = new Message() { AuthorId = currUser.Id, Date = DateTime.Now, Body = MessageText, ResId = Quotation.SaleOrder.Id };
+           // var message = new Message() { AuthorId = currUser.PartnerId, ExternalAuthorId = currUser.ExternalPartnerId, Date = DateTime.Now, Body = "<p>" + MessageText + "</p>", ResId = Quotation.SaleOrder.Id, MessageType = MessageType.comment, Model = "sale.order" };
+           var message = new Message() { AuthorId = currUser.PartnerId, ExternalAuthorId = currUser.ExternalPartnerId, Date = DateTime.Now, Body = MessageText, ResId = Quotation.SaleOrder.Id, MessageType = MessageType.comment, Model = "sale.order" };
 
            //insertion de message dans la base
            var resul = await StoreManager.MessageStore.InsertAsync(message);
 
            Quotation.Messages.Add(message);
 
-           MessageSource.Add(new MessageModel(message) { Index = messagesource.Count + 1, Name = currUser.Name });
-
-           //MessageText = null;
+           MessageSource.Add(new MessageModel(message) { Index = MessageSource.Count + 1, Name = currUser.Name });
+           MessageText = null;
        });
 
         public Command BackButton => new Command(async () =>
@@ -62,9 +61,6 @@ namespace voltaire.PageModels
             }
         }
 
-
-
-
         public async override void Init(object initData)
         {
             base.Init(initData);
@@ -74,22 +70,26 @@ namespace voltaire.PageModels
             //message recuperation from Quotation.SaleOrder.Id
             var message_list = await StoreManager.MessageStore.GetMessagesBySaleOrderIdAsync(Quotation.SaleOrder.Id);
 
-            if ((message_list.Any()) && (message_list != null))
+            if (message_list != null)
             {
-
-                List<MessageModel> message_models = new List<MessageModel>();
-
-                foreach (var item in message_list)
+                if (message_list.Any())
                 {
-                    //Name récupération
-                    var partner = await StoreManager.CustomerStore.GetCustomerByMessageAuthorIdAsync(item.AuthorId);
+                    List<MessageModel> message_models = new List<MessageModel>();
 
-                    message_models.Add(new MessageModel(item) { Index = message_models.Count + 1, Name = partner.Name });
+                    foreach (var item in message_list)
+                    {
+                        //Name récupération
+                        var partner = await StoreManager.CustomerStore.GetCustomerByMessageAuthorIdAsync(item.AuthorId);
+
+                        message_models.Add(new MessageModel(item) { Expanded = true, Index = message_models.Count + 1, Name = partner.Name });
+                    }
+                    MessageSource = new ObservableCollection<MessageModel>(message_models);
                 }
-
-                MessageSource = new ObservableCollection<MessageModel>(message_models);
             }
-
+            else
+            {
+                MessageSource = new ObservableCollection<MessageModel>();
+            }
 
 
         }
