e073acddaf6efde54a226bba5d8848347befd9b1
Vaibhav Khanna <y2kvaibhav@gmail.com>
2017-12-21 16:05:23 +0530

    Changes

--- PopUps/ToastLayout.xaml.cs
+++ PopUps/ToastLayout.xaml.cs
@@ -29,19 +29,22 @@ namespace voltaire.PopUps
         {
             Device.BeginInvokeOnMainThread( () => 
             {
-                container.Padding = 0;
-                container.LayoutTo(new Rectangle(0,0,container.Width,20),500,Easing.Linear);
-                container.HeightRequest = 20;
-                anim.HeightRequest = 20;
-                anim.WidthRequest = 20;
-                anim.HorizontalOptions = LayoutOptions.Center;
-                anim.TranslationX = -100;
-                anim.Play();
-                ToastLabel.Text = "";
-                if (!ShouldAnimate)
+                if (container != null && anim != null)
                 {
-                    ShouldAnimate = true;
-                    Animate();
+                    container.Padding = 0;
+                    container.LayoutTo(new Rectangle(0, 0, container.Width, 20), 500, Easing.Linear);
+                    container.HeightRequest = 20;
+                    anim.HeightRequest = 20;
+                    anim.WidthRequest = 20;
+                    anim.HorizontalOptions = LayoutOptions.Center;
+                    anim.TranslationX = -100;
+                    anim.Play();
+                    ToastLabel.Text = "";
+                    if (!ShouldAnimate)
+                    {
+                        ShouldAnimate = true;
+                        Animate();
+                    }
                 }
             });
             
