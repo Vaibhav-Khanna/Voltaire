ed2d9581395e7c3e457353512909018f49d7cf2d
Christophe Dospital <christophe.dospital@matsiya.com>
2018-07-12 10:32:49 +0200

    akavache fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -26,6 +26,9 @@ namespace voltaire
 
             InitializeComponent();
 
+            SQLitePCL.Batteries_V2.Init();
+            SQLitePCL.raw.FreezeProvider();
+
             ProductConstants.Init();
 
             PclStorage.Init();
